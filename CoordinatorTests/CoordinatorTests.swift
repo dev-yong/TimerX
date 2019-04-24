6f88efb92a95ac38de4e29d37ada61ae8b600e5c
이광용 <rhkdrmfh@gmail.com>
2019-04-23 20:04:00 -0400

    no message

---
+++ CoordinatorTests/CoordinatorTests.swift
@@ -0,0 +1,34 @@
+//
+//  CoordinatorTests.swift
+//  CoordinatorTests
+//
+//  Created by 이광용 on 23/04/2019.
+//  Copyright © 2019 GwangYongLee. All rights reserved.
+//
+
+import XCTest
+@testable import Coordinator
+
+internal class CoordinatorTests: XCTestCase {
+
+    override internal func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override internal func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    internal func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    internal func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
