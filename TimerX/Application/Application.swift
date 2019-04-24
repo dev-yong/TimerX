9302489a27130892b6bb1e7a7a9666cbab2f0ae1
이광용 <rhkdrmfh@gmail.com>
2019-04-23 20:06:04 -0400

    Update Foldering

---
+++ TimerX/Application/Application.swift
@@ -0,0 +1,13 @@
+//
+//  Application.swift
+//  TimerX
+//
+//  Created by 이광용 on 23/04/2019.
+//  Copyright © 2019 GwangYongLee. All rights reserved.
+//
+
+import Foundation
+
+internal final class Application {
+
+}
---
+++ TimerX/Application/Coordinator/Storyboard.swift
@@ -0,0 +1,29 @@
+//
+//  Storyboard.swift
+//  TimerX
+//
+//  Created by 이광용 on 23/04/2019.
+//  Copyright © 2019 GwangYongLee. All rights reserved.
+//
+
+import UIKit
+import Coordinator
+
+internal enum Storyboard: StoryboardProtocol {
+    case combination, timeLog
+    internal var name: String {
+        switch self {
+        case .combination: return "Combination"
+        case .timeLog: return "TimeLog"
+        }
+    }
+    internal var bundle: Bundle? {
+        return nil
+    }
+    public func `init`() -> UIStoryboard {
+        return UIStoryboard.init(name: name, bundle: bundle)
+    }
+    public func instantiate(viewController: UIViewController.Type) {
+        `init`().instantiateViewController(withIdentifier: viewController.reuseIdentifier)
+    }
+}
