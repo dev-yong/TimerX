ca942f12cbb063b9ebe26bf28dfeb8f40c14525f
이광용 <rhkdrmfh@gmail.com>
2019-05-01 23:57:29 -0400

    Update TimePickerView

--- TimerX/Common/UIVIew/TimePickerView.swift
+++ TimerX/Common/UIVIew/TimePickerView.swift
@@ -26,6 +26,13 @@ public enum TimeUnit: CustomStringConvertible {
     }
 }
 
+public protocol TimePickerViewDelegate: AnyObject {
+    func timePickerView(_ pickerView: TimePickerView,
+                        hour: Int,
+                        minute: Int,
+                        second: Int)
+}
+
 open class TimePickerView: UIPickerView {
     public enum Mode {
         case h, m, s, hm, ms, hms
@@ -48,6 +55,15 @@ open class TimePickerView: UIPickerView {
     private var modeDataSource: [TimeUnit] {
         return mode.datas
     }
+    private var time: [TimeUnit: Int] = [:] {
+        didSet {
+            timePickerViewDelegate?.timePickerView(self,
+                                                   hour: time[.hour] ?? 0,
+                                                   minute: time[.minute] ?? 0,
+                                                   second: time[.second] ?? 0)
+        }
+    }
+    public weak var timePickerViewDelegate: TimePickerViewDelegate?
     override public init(frame: CGRect) {
         super.init(frame: frame)
         initialize()
@@ -60,6 +76,32 @@ open class TimePickerView: UIPickerView {
         self.delegate = self
         self.dataSource = self
     }
+    public func set(hour: Int, minute: Int, second: Int, animated: Bool) {
+        assert(hour < 24 && minute < 60 && second < 60)
+        DispatchQueue.main.async {
+            switch self.mode {
+            case .h:
+                self.selectRow(hour, inComponent: 0, animated: animated)
+            case .m:
+                self.selectRow(minute, inComponent: 0, animated: animated)
+            case .s:
+                self.selectRow(second, inComponent: 0, animated: animated)
+            case .hm:
+                self.selectRow(hour, inComponent: 0, animated: animated)
+                self.selectRow(minute, inComponent: 1, animated: animated)
+            case .ms:
+                self.selectRow(minute, inComponent: 0, animated: animated)
+                self.selectRow(second, inComponent: 1, animated: animated)
+            case .hms:
+                self.selectRow(hour, inComponent: 0, animated: animated)
+                self.selectRow(minute, inComponent: 1, animated: animated)
+                self.selectRow(second, inComponent: 2, animated: animated)
+            }
+            self.time = [.hour: hour,
+                         .minute: minute,
+                         .second: second]
+        }
+    }
 }
 
 extension TimePickerView: UIPickerViewDataSource {
@@ -68,6 +110,11 @@ extension TimePickerView: UIPickerViewDataSource {
     }
     public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return modeDataSource[component].datas.count
+    }
+    public func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let timeUnit = modeDataSource[component]
+        let value = timeUnit.datas[row]
+        time[timeUnit] = value
     }
 }
 extension TimePickerView: UIPickerViewDelegate {
