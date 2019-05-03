46acbe7db06b9cb81590f5ed35435d45a82c0662
이광용 <rhkdrmfh@gmail.com>
2019-05-02 23:46:25 -0400

    Implement custom `SegmentControl`

--- TimerX/Common/UIVIew/UISegmentedControl/SegmentControl.swift
+++ TimerX/Common/UIVIew/UISegmentedControl/SegmentControl.swift
@@ -7,11 +7,22 @@
 //
 
 import UIKit.UISegmentedControl
+import RxSwift
+import RxCocoa
+
 class SegmentedControl: UISegmentedControl {
     func set(items: [String?], animated: Bool) {
         removeAllSegments()
         items.enumerated().forEach {
             insertSegment(withTitle: $0.element, at: $0.offset, animated: animated)
+        }
+    }
+}
+
+extension Reactive where Base: SegmentedControl {
+    var items: Binder<[String?]> {
+        return Binder(self.base) { segmentedControl, items in
+            segmentedControl.set(items: items, animated: true)
         }
     }
 }
