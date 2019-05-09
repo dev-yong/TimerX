520659f4f0cadd7abb85bcd5b4a2eb6001b1f642
이광용 <rhkdrmfh@gmail.com>
2019-05-09 08:02:57 -0400

    Add `Configuration`

---
+++ TimerX/Configuration/Configuration.swift
@@ -0,0 +1,18 @@
+//
+//  Configuration.swift
+//  TimerX
+//
+//  Created by 이광용 on 05/05/2019.
+//  Copyright © 2019 GwangYongLee. All rights reserved.
+//
+
+import UIKit
+
+enum Configuration {
+    enum Font {
+        static let cellTitle = UIFont.boldSystemFont(ofSize: 16.0)
+    }
+    enum Dimension {
+        static let inset: CGFloat = 10
+    }
+}
